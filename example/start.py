2848b7f68af32695d088983a3517842d93bd6d47
David Chappell <David.Chappell@trincoll.edu>
2019-10-10 15:14:12 -0400

    Changed configuration item SECRET to SECRET_KEY as used by Flask

--- example/start.py
+++ example/start.py
@@ -1,12 +1,16 @@
 #! /usr/bin/python3 -B
 import sys
 sys.path.append("..")
+
 from werkzeug.serving import run_simple
 from werkzeug.middleware.proxy_fix import ProxyFix
-from app import app
+
 from wsgi_door.providers import init_providers
 from wsgi_door.middleware import WsgiDoorAuth, WsgiDoorFilter
+
+from app import app
 from config import auth_client_keys
+
 auth_providers = init_providers(auth_client_keys)
 app = WsgiDoorFilter(app, protected_paths=["/admin/", "/private/"])
 app = WsgiDoorAuth(app, auth_providers, secret="j+Qs/fLkj3lkj;ljX2ljW23ljk32l4")
--- example_flask_login/start.py
+++ example_flask_login/start.py
@@ -1,12 +1,16 @@
 #! /usr/bin/python3 -B
 import sys
 sys.path.append("..")
+
 from werkzeug.serving import run_simple
 from werkzeug.middleware.proxy_fix import ProxyFix
-from app import app
+
 from wsgi_door.providers import init_providers
 from wsgi_door.middleware import WsgiDoorAuth
+
+from app import app
+
 auth_providers = init_providers(app.config['AUTH_CLIENT_KEYS'])
-app = WsgiDoorAuth(app, auth_providers, app.config['SECRET'])
+app = WsgiDoorAuth(app, auth_providers, app.config['SECRET_KEY'])
 app = ProxyFix(app, x_proto=1)
 run_simple('0.0.0.0', 5000, app)
